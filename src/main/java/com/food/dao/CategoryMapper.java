b0aa01b9a1339665fffa9a18727c163e89ba12c4
myqlrs <531353101@qq.com>
2020-12-22 16:52:14 +0800

    myqlrs:dao层相关函数的注释CategoryMapper

--- src/main/java/com/food/dao/CategoryMapper.java
+++ src/main/java/com/food/dao/CategoryMapper.java
@@ -7,17 +7,25 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 public interface CategoryMapper {
+
+    //根据主键id删除
     int deleteByPrimaryKey(Integer id);
 
+    //插入分类信息（全部）
     int insert(Category record);
 
+    //插入分类信息（非空）
     int insertSelective(Category record);
 
+    //查询所有符合example类条件的分类信息
     List<Category> selectByExample(CategoryExample example);
 
+    //根据主键id查询
     Category selectByPrimaryKey(Integer id);
 
+    //更新分类信息（非空）
     int updateByPrimaryKeySelective(Category record);
 
+    //更新分类信息（全部）
     int updateByPrimaryKey(Category record);
 }
